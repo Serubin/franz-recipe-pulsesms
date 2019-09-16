c94c0b49989c6ad90a37c035b40aed6a1afc7703
Serubin <Serubin@Serubin.net>
2019-09-15 21:40:16 -0400

    Update badge count selector

--- webview.js
+++ webview.js
@@ -1,6 +1,6 @@
 module.exports = (Franz) => {
   function getMessages() {
-    Franz.setBadge(document.querySelectorAll('unread_count').innerHTML);
+    Franz.setBadge(document.querySelector("#unread_count").innerHTML.replace(/\s/g,''));
   }
 
   // check for new messages every second and update Franz badge
